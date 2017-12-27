973cd62438b7ee53af2cd57ec95f83dc046622c6
OMGDuke <lex1989@gmail.com>
2017-12-27 23:24:06 +0000

    Can now add a fish to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import StorePicker from './StorePicker';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            fishes: {},
+            order: {}
+        }
+        this.addFish = this.addFish.bind(this);
+    }
+
+    addFish(fish) {
+        const fishes = {...this.state.fishes};
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        this.setState({fishes});
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -13,7 +28,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
